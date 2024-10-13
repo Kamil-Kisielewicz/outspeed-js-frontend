89169407a3c8f29d66f0498922481ababcb5b867
Sahil Manocha <sahilmanocha1994@gmail.com>
2024-10-13 16:18:55 -0700

    fixes

--- examples/playground-js/src/main.tsx
+++ examples/playground-js/src/main.tsx
@@ -1,9 +1,10 @@
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import "@outspeed/react/styles.css";
+import App from "./realtime-app/app.tsx";
 
 import { router } from "./router.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <RouterProvider router={router} />
+  <App />
 );
